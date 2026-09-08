370dbf81c3485857ce40d6960fb7748d61aff493
Alfred <csalfred161101@gmail.com>
2026-09-08 15:09:50 +0530

    .

--- pdfload.py
+++ pdfload.py
@@ -8,7 +8,7 @@ def load_and_chunk(filepath):
 
     splited_text = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)#splitting the text into chunks of 500 characters with an overlap of 50 characters.
 
-    chunks = text_splitter.split_documents(pages)
+    chunks = splited_text.split_documents(pages)
 
 
     print(f"Success! Broke the PDF into {len(chunks)} chunks.")
