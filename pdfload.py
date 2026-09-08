314c48b5bb42ab5ecc9123f7410f986e10ceb837
Alfred <csalfred161101@gmail.com>
2026-09-08 17:45:08 +0530

    changing chunk size

--- pdfload.py
+++ pdfload.py
@@ -6,7 +6,7 @@ def load_and_chunk(filepath):
     loader = PyPDFLoader(filepath) #loading the pdf file.
     pages = loader.load()
 
-    splited_text = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)#splitting the text into chunks of 500 characters with an overlap of 50 characters.
+    splited_text = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)#splitting the text into chunks of 500 characters with an overlap of 50 characters.
 
     chunks = splited_text.split_documents(pages)
 
